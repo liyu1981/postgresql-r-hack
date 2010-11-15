b937f60f9de4071b945e426595949f252f0602e4
Yu LI <liyu1981@gmail.com>
2010-11-15 18:28:31 +0800

    Add .gitignore and myconf.sh.

---
+++ myconf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# --enable-replication is now default option. No need to do again.
+
+# debug version
+./configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev --enable-debug --enable-cassert
+#release version
+# ./configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev
