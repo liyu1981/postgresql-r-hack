a0260c2608b955eadc32582894ca95f7f552082c
Yu LI <liyu1981@gmail.com>
2010-12-06 20:17:25 +0800

    Make the myconf.sh more robust.

--- myconf.sh
+++ myconf.sh
@@ -2,9 +2,30 @@
 
 # --enable-replication is now default option. No need to do again.
 
+if [ ! -f "configure" ];
+then
+	echo "Can not find Postgres-R configure, try autoconf ..."
+	autoconf
+	echo "OK."
+fi
+
 # debug version
 ./configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev --enable-debug --enable-cassert
-./spread/configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev --enable-debug --enable-cassert
 #release version
 # ./configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev
-# ./spread/configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev
+
+cd spread
+
+if [ ! -f "configure" ];
+then
+	echo "Can not find Spread configure, try autoconf ..."
+	autoconf
+	echo "OK."
+fi
+
+# debug version
+./configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev --enable-debug --enable-cassert
+#release version
+# ./configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev
+
+cd ..
