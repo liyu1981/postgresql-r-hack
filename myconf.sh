5696d6d51c0675584e0c4e3c1b30489e789f0b0d
Yu LI <liyu1981@gmail.com>
2010-12-09 17:37:33 +0800

    A much better myconf scripts :)
    Now with the ability to specify install-prefix, turn on/off release,
    omit postgres/spread.

--- myconf.sh
+++ myconf.sh
@@ -2,30 +2,97 @@
 
 # --enable-replication is now default option. No need to do again.
 
-if [ ! -f "configure" ];
+usage()
+{
+	echo "Usage: $0 -d <install-prefix> -r -p -s"
+	echo "  -d <install-prefix>: required : should be absolute path"
+	echo "  -r                 : option   : turn on release build"
+	echo "  -p                 : option   : turn off configure postgres-r"
+	echo "  -s                 : option   : turn off configure spread"
+	exit 1
+}
+
+config-postgres()
+{
+	if [ ! -f "configure" ];
+	then
+		echo "Can not find Postgres-R configure, try autoconf ..."
+		autoconf
+		echo "OK."
+	fi
+
+	if [ $release_flag -eq 1 ];
+	then
+		./configure --prefix=$install_prefix --exec-prefix=$install_prefix --enable-debug --enable-cassert --enable-coordinator-debug
+	else
+		./configure --prefix=$install_prefix --exec-prefix=$install_prefix
+	fi
+}
+
+config-spread()
+{
+	cd spread
+
+	if [ ! -f "configure" ];
+	then
+		echo "Can not find Spread configure, try autoconf ..."
+		autoconf
+		echo "OK."
+	fi
+
+	if [ $release_flag -eq 1 ];
+	then
+		./configure --prefix=$install_prefix --exec-prefix=$install_prefix --enable-debug --enable-cassert
+	else
+		./configure --prefix=$install_prefix --exec-prefix=$install_prefix
+	fi
+
+	cd ..
+}
+
+###########################################################################################
+# main starts here
+
+release_flag=0
+install_prefix=./postrep-dev
+postgresr_flag=1
+spread_flag=1
+
+if [ $# -lt 1 ]; 
 then
-	echo "Can not find Postgres-R configure, try autoconf ..."
-	autoconf
-	echo "OK."
+	usage
 fi
 
-# debug version
-./configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev --enable-debug --enable-cassert --enable-coordinator-debug
-#release version
-# ./configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev
+while getopts d:r opt
+do
+	case "$opt" in
+		d) install_prefix="$OPTARG";;
+		r) release_flag=1;;
+		p) postgresr_flag=0;;
+		s) spread_flag=0;;
+		\?) usage;;
+	esac
+done
 
-cd spread
+install_prefix=`readlink -f "$install_prefix"`
 
-if [ ! -f "configure" ];
+#echo $release_flag
+#echo $install_prefix
+#echo $postgresr_flag
+#echo $spread_flag
+
+if [ $postgresr_flag -eq 1 ];
 then
-	echo "Can not find Spread configure, try autoconf ..."
-	autoconf
-	echo "OK."
+	echo "start configure postgres-r"
+	config-postgres
+	echo "done."
 fi
 
-# debug version
-./configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev --enable-debug --enable-cassert
-#release version
-# ./configure --prefix=/home/yli/TestCamp/postrep-dev --exec-prefix=/home/yli/TestCamp/postrep-dev
+if [ $spread_flag -eq 1 ];
+then
+	echo "start configure spread"
+	config-spread
+	echo "done."
+fi
 
-cd ..
+echo "Bye. :)"
